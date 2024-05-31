79677f41d8535a73aab1e183ee497c538a31e500
Doug <douglazenby@gmail.com>
2024-05-31 11:20:20 +0100

    Started square root finder

---
+++ square_root_finder.py
@@ -0,0 +1,17 @@
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    if square_target < 0:
+        raise ValueError('Square root of negative number is not defined in real numbers')
+    if square_target == 1:
+        root = 1
+        print(f'The square root of {square_target} is 1')
+    elif square_target == 0:
+        root = 0
+        print(f'The square root of {square_target} is 0')
+
+    else:
+        low = 0
+        high = max(1, square_target)
+        root = None
+        
+        for _ in range(max_iterations):
+            pass
