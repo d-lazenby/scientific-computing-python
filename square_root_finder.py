89d42b5d1453616a82ad8838709d7ff822a15132
Doug <douglazenby@gmail.com>
2024-06-01 13:55:06 +0100

    Finished square_root_finder Started tower_of_hanoi_recursion.py

--- square_root_finder.py
+++ square_root_finder.py
@@ -14,4 +14,25 @@ def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
         root = None
         
         for _ in range(max_iterations):
-            pass
+            mid = (low + high) / 2
+            square_mid = mid**2
+
+            if abs(square_mid - square_target) < tolerance:
+                root = mid
+                break
+
+            elif square_mid < square_target:
+                low = mid
+            else:
+                high = mid
+
+        if root is None:
+            print(f"Failed to converge within {max_iterations} iterations.")
+    
+        else:   
+            print(f'The square root of {square_target} is approximately {root}')
+    
+    return root
+
+N = 16
+square_root_bisection(N)
