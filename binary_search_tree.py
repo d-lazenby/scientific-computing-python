ca06d39e7b4d637d7733071ab6f445dfce61640e
Doug <douglazenby@gmail.com>
2024-06-10 22:35:20 +0100

    Started binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def _insert(self, node, key):
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
