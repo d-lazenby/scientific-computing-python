a0bbd01d835c7d8852c63bbab5b6f372a943b8c5
Doug <douglazenby@gmail.com>
2024-06-17 19:43:12 +0100

    Added __str__ method.

--- challenges/budget_app.py
+++ challenges/budget_app.py
@@ -1,44 +1,111 @@
 class Category:
     
-    def __init__(self, category):
+    def __init__(self, category_name):
         self.ledger = []
-        self.category = category
+        self.category_name = category_name
     
     def __str__(self):
-        pass
+        line_length = 30
+        half_line_length = line_length / 2
+        lines = []
+        len_category = len(self.category_name)
         
+        stars_left = int((half_line_length - len_category / 2))
+        if len_category % 2 == 0:
+            stars_right = stars_left
+        else:
+            stars_right = stars_left + 1
+        lines.append(stars_left * "*" + self.category_name + stars_right * "*")
+        for i, item in enumerate(self.ledger):
+            if i == 0:
+                lines.append("deposit" + " " * 16 + f"{item['amount']:.2f}")
+            else:
+                num_padding = 7 - len(f"{item['amount']:.2f}")
+                if (len_desc := len(item['description'])) > 23:
+                    description = item['description'][:23]
+                    description_padding = 0
+                else:
+                    description = item['description']
+                    description_padding = 23 - len_desc
+                padding = int(description_padding + num_padding) * " "
+                lines.append(description + padding + f"{item['amount']:.2f}")
+        lines.append(f"Total: {self.get_balance():.2f}")
+        return "\n".join(lines)
+            
     def deposit(self, amount, description=''):
         self.ledger.append({'amount': amount, 'description': description})
         
     def withdraw(self, amount, description=''):
-        if amount > self.get_balance():
-            print("Withdrawal amount greater than remaining balance")
-            return False
-        else:
+        if self.check_funds(amount):
             self.deposit(-amount, description)
             return True
+        return False
         
     def get_balance(self):
         return sum(map(lambda tx: tx['amount'], self.ledger))
     
-    def transfer(self, destination_category, amount):
-        if type(destination_category) == type(self):
-            self.withdraw(amount, description=f"Transfer to {destination_category.category}")
-            destination_category.deposit(amount, description=f"Transfer from {self.category}")
-        else:
-            raise TypeError
-            
-            
+    def transfer(self, amount, destination_category):
+        if self.check_funds(amount):
+            destination_category.deposit(amount, description=f"Transfer from {self.category_name}")
+            self.withdraw(amount, description=f"Transfer to {destination_category.category_name}")
+            return True
+        return False
+        
+    def check_funds(self, amount):
+        if not (amount > self.get_balance()):
+            return True
+        return False
 
-testing = Category("Food")
-testing2 = Category("Clothes")
-print(testing.ledger, testing.get_balance())
-testing.deposit(1000, 'tx1')
-testing2.deposit(2000, 'tx1')
-print(testing.ledger, testing2.ledger)
-testing.transfer(testing2, 100)
-print(testing.ledger, testing2.ledger)
+def create_spend_chart(categories):
+    pass
 
+
+food = Category("Food")
+food.deposit(1000, "deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+print(food)
+
+# 
+# line_length = 30
+# half_line_length = line_length / 2
+
+# def output(budget_category):
+#     lines = []
+#     len_category = len(budget_category.category_name)
+#     stars_left = int((half_line_length - len_category / 2))
+#     if len_category % 2 == 0:
+#         stars_right = stars_left
+#     else:
+#         stars_right = stars_left + 1
+#     lines.append(stars_left * "*" + budget_category.category_name + stars_right * "*")
+#     for i, item in enumerate(budget_category.ledger):
+#         if i == 0:
+#             lines.append("initial amount" + " " * 9 + f"{item['amount']:.2f}")
+#         else:
+#             num_padding = 7 - len(f"{item['amount']:.2f}")
+#             if (len_desc := len(item['description'])) > 23:
+#                 description = item['description'][:23]
+#                 description_padding = 0
+#             else:
+#                 description = item['description']
+#                 description_padding = 23 - len_desc
+#             padding = int(description_padding + num_padding) * " "
+#             lines.append(description + padding + f"{item['amount']:.2f}")
+            
+#     assert all([len(line) == 30 for line in lines])
+            
+#     return lines
+
+# print(output(food))
+# *************Food*************
+# initial deposit        1000.00
+# groceries               -10.15
+# restaurant and more foo -15.89
+# Transfer to Clothing    -50.00
+# Total: 923.96
 
 
 
