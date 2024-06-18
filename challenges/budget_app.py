fa0bd4f3cacacdf6b158a5146a9342653f1dfc1c
Doug <douglazenby@gmail.com>
2024-06-18 18:17:01 +0100

    Finished budget_app.py

--- challenges/budget_app.py
+++ challenges/budget_app.py
@@ -5,11 +5,10 @@ class Category:
         self.category_name = category_name
     
     def __str__(self):
+        lines = []
         line_length = 30
         half_line_length = line_length / 2
-        lines = []
         len_category = len(self.category_name)
-        
         stars_left = int((half_line_length - len_category / 2))
         if len_category % 2 == 0:
             stars_right = stars_left
@@ -57,7 +56,45 @@ class Category:
         return False
 
 def create_spend_chart(categories):
-    pass
+    # Calculate percentage spend for each category
+    withdrawals = []
+    for category in categories:
+        filtered_tx = filter(lambda c: c['amount'] < 0 and "Transfer" not in c["description"], category.ledger)
+        category_withdrawals = sum(tx['amount'] for tx in filtered_tx)
+        withdrawals.append(category_withdrawals)
+        print(withdrawals)
+    total_spend = sum(withdrawals)
+    percentage_spend = [round((withdrawal / total_spend * 100) // 10) * 10 for withdrawal in withdrawals]
+    
+    # Draw chart
+    num_cols = len(categories) * 3 + 1
+    bar_chart = 'Percentage spent by category\n'
+    for ylabel in range(100, -10, -10):
+        bar_chart += str(ylabel).rjust(3) + '| '
+        for percent in percentage_spend:
+            if ylabel <= percent:
+                bar_chart += 'o  '
+            else:
+                bar_chart += '   '
+        bar_chart += '\n'
+        
+    bar_chart += '    ' + '-' * num_cols + '\n'
+
+    category_names = [category.category_name for category in categories]
+    longest_cat_name = max(len(name) for name in category_names)
+    for i in range(longest_cat_name):
+        bar_chart += '     '
+        for category_name in category_names:
+            if i < len(category_name):
+                bar_chart += category_name[i] + '  '
+            else:
+                bar_chart += '   '
+        if i == longest_cat_name - 1:
+            break
+        bar_chart += '\n'
+    
+    return bar_chart
+
 
 
 food = Category("Food")
@@ -66,4 +103,10 @@ food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
 clothing = Category("Clothing")
 food.transfer(50, clothing)
-print(food)
+
+clothing.deposit(2000, "deposit")
+clothing.withdraw(20.20, "jumper")
+clothing.withdraw(10.12, "jeans")
+
+categories = [food, clothing]
+print(create_spend_chart(categories))
